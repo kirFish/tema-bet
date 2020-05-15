af248cb99ccad72da04f48b1a6664711578a150d
kirichripper <kir.fishchuk2014@gmail.com>
2020-05-15 23:01:07 +0300

    Project skeleton and Login activity xml nearly done

---
+++ app/src/main/java/com/kirichripper/entertainment/temabet/activity/BetDetails.kt
@@ -0,0 +1,13 @@
+package com.example.temabet.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.temabet.R
+
+class BetDetails : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bet_details)
+    }
+}
---
+++ app/src/main/java/com/kirichripper/entertainment/temabet/activity/History.kt
@@ -0,0 +1,13 @@
+package com.example.temabet.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.temabet.R
+
+class History : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+    }
+}
