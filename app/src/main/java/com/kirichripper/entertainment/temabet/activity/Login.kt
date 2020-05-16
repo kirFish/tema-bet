8657f6917917a8500a619e30f899d0292dc30081
kirichripper <kir.fishchuk2014@gmail.com>
2020-05-16 19:21:50 +0300

    changed the appearance of the login page and styles a bit

--- app/src/main/java/com/kirichripper/entertainment/temabet/activity/Login.kt
+++ app/src/main/java/com/kirichripper/entertainment/temabet/activity/Login.kt
@@ -2,12 +2,18 @@ package com.example.temabet.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import com.example.temabet.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+       // while(check)
     }
-}
+
+    fun checkCredintalsStatus() : Boolean = inputLogin.text.isEmpty() && inputPassword.text.isEmpty()
+ }
