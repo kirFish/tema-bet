4ee2dbcbef49210314f3f88ac1a7dfe0a975a396
tema3210 <play.aab.25122001@gmail.com>
2020-05-21 02:55:00 +0300

    remove redundant data class, implement service auth.  req. (but not resp. processing)

--- app/src/main/java/com/kirichripper/entertainment/temabet/service/Data.kt
+++ app/src/main/java/com/kirichripper/entertainment/temabet/service/Data.kt
@@ -2,17 +2,21 @@ package com.example.temabet.service
 
 import android.app.Service
 import android.content.Intent
-import android.os.Binder
 import android.os.IBinder
-import android.os.IInterface
-import android.os.Parcel
-import java.io.FileDescriptor
-import java.io.PrintWriter
+import com.example.temabet.R
+import org.json.JSONObject
+//import sun.net.www.http.HttpClient
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
+import java.nio.charset.StandardCharsets
+import java.security.MessageDigest
+
 
 //const val adress = "127.0.0.1:8080"
 
 class Data : Service() {
-
+    private var userId: Int? = null;
 //    class DataBinder : Binder() {
 //        override fun onTransact(code: Int, data: Parcel, reply: Parcel?, flags: Int): Boolean {
 //            return super.onTransact(code, data, reply, flags)
@@ -32,7 +36,32 @@ class Data : Service() {
     //This is where the intent from start service goes.
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val supErrC = super.onStartCommand(intent, flags, startId) //error code
-        supErrC
+        //return supErrC
+        //val init: ServiceInitMsg? = intent?.getParcelableExtra("serviceInit")
+
+        val Login: String? = intent?.getParcelableExtra("login")
+        val password: String? = intent?.getParcelableExtra("password")
+
+        var threadHandle = Thread {
+            val url = URL(getString(R.string.serverUrl))
+
+            with(url.openConnection() as HttpURLConnection) {
+                requestMethod = "GET"
+                val reqBody = JSONObject()
+                reqBody.put("login",Login)
+                val digest: MessageDigest = MessageDigest.getInstance("SHA-256")
+                val encHash = digest.digest(password!!.toByteArray(StandardCharsets.UTF_8))
+                reqBody.put("passwh",encHash)
+
+                val wr = OutputStreamWriter(outputStream)
+                wr.write(reqBody.toString())
+                wr.flush()
+
+                //TODO: process response, send it to UI thr.
+            }
+
+        }
+
     }
 
     override fun onDestroy() {
