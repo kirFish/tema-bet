39cdb0c711884035149bff2f078f3983eaef9f6f
tema3210 <play.aab.25122001@gmail.com>
2020-05-20 13:51:48 +0300

    Service lifecycle hooks (empty) with details noted.

--- app/src/main/java/com/kirichripper/entertainment/temabet/service/Data.kt
+++ app/src/main/java/com/kirichripper/entertainment/temabet/service/Data.kt
@@ -6,6 +6,18 @@ import android.os.IBinder
 
 class Data : Service() {
 
+    override fun onCreate() {
+
+    }
+
+    override fun onDestroy() {
+
+    }
+    //Called if all activities dead
+    override fun onUnbind(intent: Intent?): Boolean {
+        return false
+    }
+    //called ONCE, for getting IBinder handler
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
     }
