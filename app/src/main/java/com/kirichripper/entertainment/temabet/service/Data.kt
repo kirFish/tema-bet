071e61db13deda347f31d43cb7a1908631b500a4
tema3210 <play.aab.25122001@gmail.com>
2020-05-20 18:13:28 +0300

    Add Service Init Message data type

--- app/src/main/java/com/kirichripper/entertainment/temabet/service/Data.kt
+++ app/src/main/java/com/kirichripper/entertainment/temabet/service/Data.kt
@@ -2,16 +2,41 @@ package com.example.temabet.service
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
+import android.os.IInterface
+import android.os.Parcel
+import java.io.FileDescriptor
+import java.io.PrintWriter
+
+//const val adress = "127.0.0.1:8080"
 
 class Data : Service() {
 
+//    class DataBinder : Binder() {
+//        override fun onTransact(code: Int, data: Parcel, reply: Parcel?, flags: Int): Boolean {
+//            return super.onTransact(code, data, reply, flags)
+//        }
+//
+//        override fun dump(fd: FileDescriptor, fout: PrintWriter, args: Array<out String>?) {
+//            super.dump(fd, fout, args)
+//        }
+//    }
+
+
     override fun onCreate() {
+        super.onCreate()
 
     }
 
-    override fun onDestroy() {
+    //This is where the intent from start service goes.
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val supErrC = super.onStartCommand(intent, flags, startId) //error code
+        supErrC
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
     }
     //Called if all activities dead
     override fun onUnbind(intent: Intent?): Boolean {
