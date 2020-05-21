af37e26f3bb5049cf15b687513713bdac766ad40
kirichripper <kir.fishchuk2014@gmail.com>
2020-05-22 00:52:44 +0300

    initialized main login activity methods

--- app/src/main/java/com/kirichripper/entertainment/temabet/activity/Login.kt
+++ app/src/main/java/com/kirichripper/entertainment/temabet/activity/Login.kt
@@ -3,45 +3,63 @@ package com.example.temabet.activity
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.temabet.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
 
-    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         startCredentialsCheck()
-
     }
 
 
     private fun startCredentialsCheck() {
 
         //Because by default edit text contains zero
+        var passwordReady = false;
+        var loginReady = false;
         var ifCredentialsReady = false;
 
-       // while(!ifCredentialsReady){
-
-//            if(checkPassword()) && checkLogin()){
-//                ifCredentialsReady = true;
+//        while(!ifCredentialsReady){
+//
+//            loginReady = checkLogin()
+//            passwordReady = checkPassword()
+//
+//            if(loginReady){
+//                inputLogin.highlightColor = getColor(R.color.editTextCorrect)
 //            }
-        //}
+//
+//            if(passwordReady){
+//                inputPassword.highlightColor = getColor(R.color.editTextCorrect)
+//            }
+//
+//            if(loginReady && passwordReady){
+//                ifCredentialsReady = true
+//            }
+//        }
     }
 
-    private fun checkLogin(): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    private fun checkPassword(): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    public fun startSignIn(view : android.view.View){
+    private fun checkLogin(): Boolean = inputLogin.text.isEmpty() && (inputLogin.text.length < 45)
 
 
-        //this.startService()
+    private fun checkPassword(): Boolean = inputPassword.text.isEmpty()
+
+
+    fun startSignIn(view : android.view.View){
+
+        if(checkLogin() && checkPassword()){
+            Toast.makeText(applicationContext,"Check input data",Toast.LENGTH_LONG).show()
+        }else{
+
+            mainLoginLayout.visibility = View.INVISIBLE;
+            secondaryLoginLayout.visibility = View.VISIBLE;
+            //this.startService()
+        }
     }
 
 
