9cdb0035ec094981dfabbbecdaea6bf4cc9544c1
tema3210 <play.aab.25122001@gmail.com>
2020-05-22 15:56:17 +0300

    implement service

--- app/src/main/java/com/kirichripper/entertainment/temabet/service/Data.kt
+++ app/src/main/java/com/kirichripper/entertainment/temabet/service/Data.kt
@@ -1,16 +1,18 @@
 package com.kirichripper.entertainment.temabet.service
 
-import android.annotation.SuppressLint
 import android.app.Service
 import android.content.Intent
-import android.os.Binder
-import android.os.IBinder
-import android.os.IInterface
-import android.os.Parcel
+import android.os.*
+
 import android.util.JsonReader
 import com.example.temabet.R
+import kotlinx.android.parcel.Parcelize
+import kotlinx.coroutines.Dispatchers
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
 import org.json.JSONObject
-import java.io.FileDescriptor
+
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.net.HttpURLConnection
@@ -19,30 +21,50 @@ import java.nio.charset.StandardCharsets
 import java.security.MessageDigest
 
 
+import kotlinx.coroutines.launch
 
-class Data : Service() {
+//@Parcelize
+//data class UserDataMap(
+//    val map: Map<String, Parcelable?> = hashMapOf()
+//) : Parcelable, Map<String, Parcelable?> by map
 
+final class Data : Service() {
+
+    private var task: Job? = null
     private var userId: Int? = null;
+    private var error: Int? = null;
+    private var arb_err: String? = null;
+
+    var done = false;
+
+    private fun user(): Bundle? {
+        if (done) {
+            val resp = Bundle()
+            resp.putString("arb_err",arb_err)
+            if (error != null) resp.putInt("error",error!!)
+            if (userId != null) resp.putInt("id",userId!!)
+
+            //return UserDataMap(mapOf<String,Parcelable?>("id" to userId,"error" to error, "arb_err" to aerrB))
+            return resp
+        }
+        else return null
+    }
+
 
     override fun onCreate() {
         super.onCreate()
-
     }
-    //TUT POLNYI PIZDEC NAPISAL TEMA2
 
     //This is where the intent from start service goes.
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        /*val supErrC =*/ super.onStartCommand(intent, flags, startId) //error code
+        /*val supErrC =*/
+        super.onStartCommand(intent, flags, startId) //error code
 
-        //return supErrC
-        //val init: ServiceInitMsg? = intent?.getParcelableExtra("serviceInit")
+        this.task = GlobalScope.launch(Dispatchers.IO) {
+            val Login: String? = intent?.getStringExtra("login")
+            val password: String? = intent?.getStringExtra("password")
 
-        val Login: String? = intent?.getStringExtra("login")
-        val password: String? = intent?.getStringExtra("password")
-
-        var threadHandle = Thread {
-            val url = URL(getString(R.string.serverUrl) + getString(R.string.loginHandler))
-
+            val url = URL("http",getString(R.string.serverUrl),"8080")
             with(url.openConnection() as HttpURLConnection) {
                 requestMethod = "GET"
                 val reqBody = JSONObject()
@@ -60,33 +82,28 @@ class Data : Service() {
 
                 when (code) {
                     403 -> {
-                        //TODO: Bad request
-
+                        error = code
                     }
                     500 -> {
-                        //TODO: ISE
+                        error = code
                     }
                     200 -> {
                         val respBodyReader = JsonReader(InputStreamReader(ist))
                         if (respBodyReader.nextName() == "id") {
                             val id: Int? = respBodyReader.nextInt()
                             userId = id!!
-
                         } else {
-                            //TODO: malformed json response in Ok response
-
-
+                            arb_err = "malformed json response in Ok response"
                         }
-
                     }
                     else -> {
-                        //TODO: unexpected http code
+
+                        arb_err = "unexpected http code"
                     }
                 };
             }
-
+            done = true;
         }
-
         return START_STICKY
     }
 
@@ -95,53 +112,39 @@ class Data : Service() {
     override fun onDestroy() {
         super.onDestroy()
     }
+
     //Called if all activities dead
     override fun onUnbind(intent: Intent?): Boolean {
         return false
     }
     //called ONCE, for getting IBinder handler
     override fun onBind(intent: Intent): IBinder {
-            return Binder()
-//        return object: IBinder {
-//            override fun getInterfaceDescriptor(): String? {
-//                return "RunsSrv service"
-//            }
-//
-//            override fun isBinderAlive(): Boolean {
-//                //TODO: implement this in proper way
-//                return true
-//            }
-//
-//            override fun linkToDeath(recipient: IBinder.DeathRecipient, flags: Int) {
-//                //??????
-//            }
-//
-//            override fun queryLocalInterface(descriptor: String): IInterface? {
-//                //TODO("Not yet implemented")
-//                return null
-//            }
-//
-//            override fun transact(code: Int, data: Parcel, reply: Parcel?, flags: Int): Boolean {
-//                TODO("Not yet implemented")
-//            }
-//
-//            override fun dumpAsync(fd: FileDescriptor, args: Array<out String>?) {
-//                TODO("Not yet implemented")
-//            }
-//
-//            override fun dump(fd: FileDescriptor, args: Array<out String>?) {
-//                TODO("Not yet implemented")
-//            }
-//
-//            override fun unlinkToDeath(recipient: IBinder.DeathRecipient, flags: Int): Boolean {
-//                TODO("Not yet implemented")
-//            }
-//
-//            override fun pingBinder(): Boolean {
-//                TODO("Not yet implemented")
-//            }
-//
-//        }
+        //return Binder()
+        val self = this;
+
+        return object: Binder() {
+            override fun onTransact(code: Int, data: Parcel, reply: Parcel?, flags: Int): Boolean {
+                if (flags != 0) return false
+                if (reply == null) return false
+
+                //return super.onTransact(code, data, reply, flags)
+                when (code) {
+                    2->  {
+                        val d = self.user()
+                        if (d == null) {
+                            reply.writeString("wait")
+                        } else {
+                            d.writeToParcel(reply, 0)
+                        }
+                        return true
+                    }
+                    else -> {
+                        return false
+                    }
+                }
+
+            }
+        }
     }
 
 }
